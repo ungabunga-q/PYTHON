19fdcb05a612370029a13de731892be9d5a2836a
Aishwarya Nevrekar <your.email@example.com>
2025-07-19 17:09:45 +0530

    git add . git commit -m "Add new file for DAY 8 and update SEC_2.PY"

---
+++ DAY 8/p8.py
@@ -0,0 +1,26 @@
+"""
+17th JULY 2025
+WHAT IS A SET?
+1. A set is an unordered collection of unique elements.
+2. It removes duplicates automatically.
+3. It is mutable, meaning you can add or remove elements after creation.
+4. Elements inside a set must be immutable ( eg: numbers, strings, tuples)
+5. Sets are defined using:
+        - Curly braces: {}
+        - set() constructor
+6. Sets support mathematical operations like:
+    - Union
+    - Intersection
+    - Difference
+    - Symmentric Difference
+    
+
+
+"""
+
+#-----------------------------------------------------------------
+# Defining a set
+#-----------------------------------------------------------------
+
+#1. Example 1 : Define a set using curly braces
+my_set={1,2,3};print(f"Example 1: {my_set}")

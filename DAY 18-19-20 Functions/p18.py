29319276477d5c994c69a846013ed324705883b7
Aishwarya Nevrekar <your.email@example.com>
2025-08-16 16:42:48 +0530

    git add . commit

---
+++ DAY 18-19-20 Functions/p18.py
@@ -0,0 +1,39 @@
+"""
+Introduction to Functions:
+
+--> What is a function?
+
+A function is a block of reuseable code that performs a specific task.
+
+Instead of writing the same code again and again, we can write it
+once inside a function and call it whenevr needed.
+
+
+--> Importance of functions in programming.
+
+A. Breaks large programs into smaller parts.
+B. Increases readability and maintainability.
+C. Encourages code reuse.
+
+
+--> Advantages:
+a. Code Reuseability - write once, use many times
+b. Readability - Clean and understandable cpde
+c. Modularity - Breals programs into smaller logical parts
+d. Easy Debugging - Errors can easily be identified into specific functions.
+
+
+
+--> Function naming rules
+a. Must start with a letter or underscore
+b. Cannot start with a digit
+c. Can contain letters ,digit, underscores
+d. Avoid Python keywords [ def , for, while , etc...]
+
+
+--> Indentation Importance:
+a. Python uses identation to define block of code
+b. 
+
+
+"""
